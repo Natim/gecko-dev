0299127344b0eb56b932b17b9611c821b0c34eff
Mathieu Leplatre <mathieu@mozilla.com>
2016-03-02 17:26:31 +0100

    Fix build' g

--- toolkit/mozapps/extensions/nsBlocklistService.js
+++ toolkit/mozapps/extensions/nsBlocklistService.js
@@ -14,6 +14,7 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/AppConstants.jsm");
 Components.utils.import("resource://services-common/KintoAddonBlocklist.js");
+Components.utils.import("resource://services-common/KintoPluginBlocklist.js");
 
 //XXX debug/hack:
 const console = (Components.utils.import("resource://gre/modules/Console.jsm", {})).console;
